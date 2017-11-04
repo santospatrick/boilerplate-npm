cd23252c6ff1de35bc5bab0b813f69c82673d4b2
Patrick Santos <jvpspt.ps@gmail.com>
2017-11-03 22:57:45 -0200

    add eslint & compatible browsers config

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
-var unique = require('uniq');
+import unique from 'uniq';
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+var copiedData = [...data];
 
 console.log('some es6 scripts & UMD example');
-console.log(...data);
+console.log(unique(copiedData));
+
+// Eslint max line length rule
+var foo = { 'bar': 'This is a bar.', 'baz': { 'qux': 'This is a qux' }, 'difficult': 'to read' };
+
+// Only to show browser support error
+fetch('https://api.github.com/users/santospatrick')
+  .then(resp => resp.json())
+  .then(me => console.table(me));
